9556d9e66a672ad28f6ece17be1d4b685466af27
jkurisu <jkurisu@yahoo.com>
2017-04-18 13:33:19 -0700

    Check if the server exists before creating it.  This allows for docker stop/start.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
 
-set -ex; \
-    echo ${SERVER_NAME}; \
-    ${LIBERTY_HOME}/bin/server create ${SERVER_NAME};
-    
+set -x;
+echo ${SERVER_NAME};
+
+${LIBERTY_HOME}/bin/server status ${SERVER_NAME}
+if [ "$?" -eq "2" ]; then
+
+${LIBERTY_HOME}/bin/server create ${SERVER_NAME};
 mv -f ${SERVER_LOCATION}/${SERVER_XML}             ${LIBERTY_HOME}/usr/servers/${SERVER_NAME}/${SERVER_XML}
 mv -f ${SERVER_LOCATION}/${SERVER_ENV}             ${LIBERTY_HOME}/usr/servers/${SERVER_NAME}/${SERVER_ENV}
 mv -f ${SERVER_LOCATION}/${APPLICATION_PROPERTIES} ${LIBERTY_HOME}/usr/servers/${SERVER_NAME}/${APPLICATION_PROPERTIES}
 mv -f ${SERVER_LOCATION}/${JVM_OPTIONS}            ${LIBERTY_HOME}/usr/servers/${SERVER_NAME}/${JVM_OPTIONS}
 mv -f ${SERVER_LOCATION}/${WAR_NAME}               ${LIBERTY_HOME}/usr/servers/${SERVER_NAME}/apps/${WAR_NAME}
-    
+
+fi
 
 ${LIBERTY_HOME}/bin/server run ${SERVER_NAME}
